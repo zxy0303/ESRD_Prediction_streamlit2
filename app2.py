ed8a8a88d1a76e7ee7bf8e6754f9e61e2fd844b4
Xinyuan Zhao <81060612+zxy0303@users.noreply.github.com>
2025-12-10 18:23:47 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -24,7 +24,7 @@ def load_models():
     # 加载 9 特征模型 (请确保你有这些文件，且文件名正确)
     try:
         models_9 = {
-            1: joblib.load('./nine_features/catboost_op_sm_1yr.pkl'),
+            1: joblib.load('./nine_features/catboost_1yr.pkl'),
             3: joblib.load('./nine_features/catboost_op_sm_3yr.pkl'),
             5: joblib.load('./nine_features/catboost_op_sm_5yr.pkl')
         }
@@ -232,4 +232,5 @@ with right_col:
             # 调试辅助：如果报错，打印当前 DataFrame 的列名，方便对比模型需求
 
             st.write("Current Input Columns:", input_data.columns.tolist())
+
 
